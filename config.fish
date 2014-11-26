be190697f9681e6742aece696e0a441792b09c63
Kyle Bolton <kyle@crashlytics.com>
2014-11-26 18:50:01 -0500

    Path and Alias additions

--- config.fish
+++ config.fish
@@ -1,7 +1,15 @@
-set -x PATH /usr/local/bin ~/bin $PATH
+set -x GOPATH /crashlytics/go
+set -x PATH /usr/local/bin /usr/local/share/npm/bin ~/bin $GOPATH/bin $PATH
 set -x EDITOR "mate -w"
 
 # RBENV
 set PATH $HOME/.rbenv/bin $PATH
 set PATH $HOME/.rbenv/shims $PATH
 rbenv rehash >/dev/null ^&1
+
+# Alias
+alias be="bundle exec"
+alias l="ls -lha"
+alias cls="cd /crashlytics"
+alias www="cd /crashlytics/www"
+alias chef="cd /crashlytics/hosted-chef"
